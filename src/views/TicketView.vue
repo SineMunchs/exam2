529dcfb029b4cbaa307a4375b92124647d924ebf
SineMunchs <167076042+SineMunchs@users.noreply.github.com>
2024-06-18 22:56:51 +0200

    commit

--- src/views/TicketView.vue
+++ src/views/TicketView.vue
@@ -1,30 +1,36 @@
 <template>
-    <header>      <img src="../../public/afsender.png" alt="HomeView.vue" class="h-6 ml-20 mt-4">
-</header>
+    <header>
+        <img src="../../public/afsender.png" alt="HomeView.vue" class="h-6 ml-20 mt-4">
+    </header>
     <div class="flex flex-row w-full justify-center items-center">
-       <div class="grid border border-black flex-grow m-20 mt-3 w-full relative">
-         <div class="p-1">
-           <div class="container p-4 mt-10">
-           <div class="mb-20">
-               <h1 class="hanken font-light text-5xl"> Reserve Free Ticket</h1>
-               <p class=" hanken font-light text-3xl ">Blågårds Plads Cph N ____ 7:30pm  25 - 29/06 2024
-               </p>
-           </div>
-  
-  
-  
-          <div class=" flex ml-20 space-x-20 mb-40">
-               <div class="border border-black p-20 custom-width text-7xl text-[#5C442B] cursor-pointer hover:text-[#EFB34B]"><a href="https://www.eventim-light.com/dk/a/6268ebc70e2f6b59c13b0fac/e/6656fcdfe820523242408b0f" target="blank">25</a></div>
-               <div class="border border-black p-20 custom-width text-7xl text-[#5C442B] cursor-pointer hover:text-[#EFB34B]"><a href="https://www.eventim-light.com/dk/a/6268ebc70e2f6b59c13b0fac/e/6656fcdfe820523242408b1b" target="blank">27</a></div>
-               <div class="border border-black p-20 custom-width text-7xl text-[#5C442B] cursor-pointer hover:text-[#EFB34B]"><a href="https://www.eventim-light.com/dk/a/6268ebc70e2f6b59c13b0fac/e/6656fcdfe820523242408b16" target="blank">26</a></div>
-               <div class="border border-black p-20 custom-width text-7xl text-[#5C442B] cursor-pointer hover:text-[#EFB34B]"><a href="https://www.eventim-light.com/dk/a/6268ebc70e2f6b59c13b0fac/e/6656fcdfe820523242408b20" target="blank">28</a></div>
-               <div class="border border-black p-20 custom-width text-7xl text-[#5C442B] cursor-pointer hover:text-[#EFB34B]"><a href="https://www.eventim-light.com/dk/a/6268ebc70e2f6b59c13b0fac/e/6656fcdfe820523242408b25" target="blank">29</a></div>
-           </div>
-       </div>
-   </div>
-           <img src="../../public/images/frei.svg" alt="HomeView.vue" class="-mr-2 mb-4">
-         </div>
-         </div>
-   </template>
-  
-   
+        <div class="grid border border-black flex-grow m-20 mt-3 w-full relative">
+            <div class="p-1">
+                <div class="container p-4 mt-10">
+                    <div class="mb-20">
+                        <h1 class="hanken font-light text-5xl">Reserve Free Ticket</h1>
+                        <p class="hanken font-light text-3xl">Blågårds Plads Cph N ____ 7:30pm 25 - 29/06 2024</p>
+                    </div>
+
+                    <div class="flex justify-center space-x-10 m-4 mb-40">
+                        <div class="border border-black p-20 custom-width text-7xl text-[#5C442B] cursor-pointer hover:text-[#EFB34B]">
+                            <a href="https://www.eventim-light.com/dk/a/6268ebc70e2f6b59c13b0fac/e/6656fcdfe820523242408b0f" target="blank">25</a>
+                        </div>
+                        <div class="border border-black p-20 custom-width text-7xl text-[#5C442B] cursor-pointer hover:text-[#EFB34B]">
+                            <a href="https://www.eventim-light.com/dk/a/6268ebc70e2f6b59c13b0fac/e/6656fcdfe820523242408b1b" target="blank">27</a>
+                        </div>
+                        <div class="border border-black p-20 custom-width text-7xl text-[#5C442B] cursor-pointer hover:text-[#EFB34B]">
+                            <a href="https://www.eventim-light.com/dk/a/6268ebc70e2f6b59c13b0fac/e/6656fcdfe820523242408b16" target="blank">26</a>
+                        </div>
+                        <div class="border border-black p-20 custom-width text-7xl text-[#5C442B] cursor-pointer hover:text-[#EFB34B]">
+                            <a href="https://www.eventim-light.com/dk/a/6268ebc70e2f6b59c13b0fac/e/6656fcdfe820523242408b20" target="blank">28</a>
+                        </div>
+                        <div class="border border-black p-20 custom-width text-7xl text-[#5C442B] cursor-pointer hover:text-[#EFB34B]">
+                            <a href="https://www.eventim-light.com/dk/a/6268ebc70e2f6b59c13b0fac/e/6656fcdfe820523242408b25" target="blank">29</a>
+                        </div>
+                    </div>
+                </div>
+                <img src="../../public/images/frei.svg" alt="HomeView.vue" class="-mr-2 mb-4">
+            </div>
+        </div>
+    </div>
+</template>
